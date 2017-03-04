0a1f4bc065aecf7fb2e56cd9648c1a1840d0db3d
upanan82 <upanan82@gmail.com>
2017-03-04 22:59:24 +0200

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    app = express(),
+    upload = require('multer')(),
+    path = require('path');
+
+// Listen port
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+// Include home page
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// Output result
+app.post('/file-size', upload.array('file'), function (req, res) {
+    res.send(JSON.stringify({size: req.files[0].size}));
+});
+
+
